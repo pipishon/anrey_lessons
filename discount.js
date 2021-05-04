7cc85f6bedc8a7100f6997f3e90f59888ab7dd9e
ZODD88 <83349484+ZODD88@users.noreply.github.com>
2021-05-04 21:13:49 +0300

    super mega commit

--- discount.js
+++ discount.js
@@ -1,4 +1,4 @@
-function discount(price, quantity) {
-}
-
-module.exports = discount
+function discount(price, quantity)
+{if (price<50) return 0;
+else if (price>50) return 10;}
+module.exports = discount; 
