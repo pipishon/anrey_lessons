1294cd8e083e086587de805cfbdfbfdd5e75cfff
ZODD88 <83349484+ZODD88@users.noreply.github.com>
2021-05-11 13:25:26 +0300

    Дильдокоммит

--- discount.js
+++ discount.js
@@ -1,6 +1,6 @@
 function discount(price, quantity)
-{
-  if (price<=50) return 0;
-  else if (price>50) return 10;
-}
+{ if (price > 500 || quantity > 50) return 25;
+  if (price > 300 && quantity > 10) return 20; 
+  if (price > 300 && quantity < 10) return 15
+  if (price > 50) return 10; else return 0;}
 module.exports = discount; 
